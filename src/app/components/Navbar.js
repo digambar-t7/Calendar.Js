b7d76d18717aa6e536a95dabc8aefe68a8820e8b
Digambar Tupurwadi <digz@kali>
2022-01-16 13:02:57 +0530

    created navbar component

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav id='Navbar'>
+            <u>
+                <li><a >Home</a></li>
+                <li><a >Events</a></li>
+                <li><a >Login</a></li>
+                <li><a >Admin</a></li>
+            </u>
+        </nav>
+    )
+}
+
+export default Navbar
